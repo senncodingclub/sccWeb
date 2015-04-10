784c6629bff577362ba458b1903390f83f04455b
rakeshdas1 <rakeshdaskowshik@gmail.com>
2015-04-09 20:28:55 -0500

    Finished parsing gh

--- js/getRakeshGH.js
+++ js/getRakeshGH.js
@@ -1,6 +1,18 @@
 $(document).ready(function(){
-    var json = "https://api.github.com/users/rakeshdas1/repos";
-    $.each(JSON.parse(json), function(idx, obj) {
-        $("#repos").append("<p>"+obj.name+"</p>");
+
+    $.ajax({
+        type: 'GET',
+        url: 'https://api.github.com/users/rakeshdas1/repos',
+        data: {get_param: 'value'},
+        dataType: 'json',
+        success: function (data) {
+           $.each(data, function(idx, obj) {
+               $("#repos").append("<a href=" + obj.html_url + "><b><p>"+obj.name+"</p></b></a>");
+               $("#repos").append("<a href=" + obj.owner.html_url + ">" + "<p>"+obj.owner.login+"</p>" + "</a>");
+               $("#repos").append("<p>"+obj.description+"</p>");
+        });
+        }
     });
+
+
 });
